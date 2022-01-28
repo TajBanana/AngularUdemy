f68f954f781f20c09f1714179d0d3c8477bf1afd
TajBanana <ytahjian@gmail.com>
2022-01-28 21:37:47 +0800

    section2-assignment3

--- section02-assignment3/src/app/app.component.ts
+++ section02-assignment3/src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayOn = true;
+  countArray = [];
+  count = 0;
+
+  onDisplayDetails() {
+    if (this.displayOn) {
+      this.displayOn = false;
+    } else {
+      this.displayOn = true;
+    }
+    this.count += 1;
+    this.countArray.push(this.count);
+    console.log('onDisplayDetails clicked.' + ' displayOn= ' + this.displayOn)
+  }
+
 }
