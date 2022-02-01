8d614a41fb20d59bd9ea1fd91b60e1d91bf51122
TajBanana <ytahjian@gmail.com>
2022-02-02 01:37:26 +0800

    course project

--- courseproject/src/app/recipes/recipes-list/recipes-list.component.ts
+++ courseproject/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Baklava','Turkish dessert','https://upload.wikimedia.org/wikipedia/commons/8/8f/Baklava_-_Turkish_special%2C_80-ply.JPEG')
+  ];
 
   constructor() { }
 
