70ee86f24e6434e796c9195effde108c6e156fee
TajBanana <ytahjian@gmail.com>
2022-02-02 12:01:55 +0800

    course project, waiting for input output of component

--- courseproject/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ courseproject/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
